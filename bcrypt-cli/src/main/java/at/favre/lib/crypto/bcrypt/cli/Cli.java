6f35e796129674ef88745cd86a94401453955fb0
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2018-07-16 18:11:54 +0200

    Fix checkstyle issue
    refs #5

--- bcrypt-cli/src/main/java/at/favre/lib/crypto/bcrypt/cli/Cli.java
+++ bcrypt-cli/src/main/java/at/favre/lib/crypto/bcrypt/cli/Cli.java
@@ -2,7 +2,10 @@ package at.favre.lib.crypto.bcrypt.cli;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
-public class Cli {
+public final class Cli {
+    private Cli() {
+    }
+
     public static void main(String[] args) {
         System.out.println(BCrypt.withDefaults().hashToString(4, "asdasd".toCharArray()));
     }
